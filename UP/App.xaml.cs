fc0e96f30ee4968bc0f24cf4c5ce411ce721a443
koukoro <lopatins425@gmail.com>
2026-06-17 16:10:23 +0700

    Добавил страницу с quiz + внёс правки в App

--- UP/App.xaml.cs
+++ UP/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using UP;
 
-namespace UP
+namespace QuizOrDie;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
--- UP/Views/QuizPage.xaml.cs
+++ UP/Views/QuizPage.xaml.cs
@@ -1,9 +1,102 @@
-namespace UP.Views;
+using QuizOrDie.ViewModels;
+using UP.ViewModels;
+using UP.Views;
+
+namespace QuizOrDie.Views;
 
 public partial class QuizPage : ContentPage
 {
-	public QuizPage()
-	{
-		InitializeComponent();
-	}
+    private readonly QuizViewModel _vm;
+
+    public QuizPage(QuizViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = _vm = vm;
+
+        _vm.OnCrashTriggered += OnCrashTriggered;
+        _vm.OnRewardTriggered += OnRewardTriggered;
+        _vm.OnAchievementUnlocked += OnAchievementUnlocked;
+
+        _vm.PropertyChanged += (s, e) =>
+        {
+            if (e.PropertyName == nameof(QuizViewModel.IsGameOver) && _vm.IsGameOver)
+                ShowGameOver();
+        };
+    }
+
+    // жизненный цикл
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _vm.InitializeAsync();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _vm.OnCrashTriggered -= OnCrashTriggered;
+        _vm.OnRewardTriggered -= OnRewardTriggered;
+        _vm.OnAchievementUnlocked -= OnAchievementUnlocked;
+    }
+
+    // обработчики событий
+
+    private async void OnCrashTriggered(int crashType)
+    {
+        MainContent.IsEnabled = false;
+
+        // ищем оверлей явно по типу — обходим проблему с x:Name
+        var overlay = this.FindByName<CrashOverlay>("CrashOverlayView");
+        if (overlay != null)
+            await overlay.ShowAsync(crashType);
+
+        MainContent.IsEnabled = true;
+        _vm.ContinueAfterOverlay();
+    }
+
+    private void OnRewardTriggered(int rewardType, string text)
+    {
+        RewardLabel.Text = text;
+        RewardOverlay.IsVisible = true;
+        MainContent.IsEnabled = false;
+    }
+
+    private async void OnAchievementUnlocked(Models.Achievement ach)
+    {
+        // показываем уведомление
+        await DisplayAlertAsync("Новая ачивка!", $"{ach.Emoji} {ach.Title}", "Ок");
+    }
+
+    // кнопки оверлеев
+
+    private void OnContinueClicked(object sender, EventArgs e)
+    {
+        RewardOverlay.IsVisible = false;
+        MainContent.IsEnabled = true;
+        _vm.ContinueAfterOverlay();
+    }
+
+    private void OnRestartClicked(object sender, EventArgs e)
+    {
+        GameOverOverlay.IsVisible = false;
+        _vm.RestartGame();
+    }
+
+    private async void OnMenuClicked(object sender, EventArgs e)
+    {
+        GameOverOverlay.IsVisible = false;
+        await Shell.Current.GoToAsync("//MainMenu");
+    }
+
+    // экран конца игры
+
+    private void ShowGameOver()
+    {
+        var result = _vm.GameResult;
+        GameOverTitle.Text = result.CorrectAnswers >= result.TotalQuestions / 2
+            ? "Молодец!" : "Провал!";
+        GameOverScore.Text = $"Очков: {_vm.Score}";
+        GameOverOverlay.IsVisible = true;
+    }
 }
