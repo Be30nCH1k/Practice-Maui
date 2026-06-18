075d3b34dfbd3402a98a3864a198464763eba4a9
dudin-dn <dudindanil592@gmail.com>
2026-06-18 10:15:13 +0700

    Добавил конвертор

--- UP/Converters/BoolToColorConverter.cs
+++ UP/Converters/BoolToColorConverter.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
 
-namespace UP.Converters
+namespace QuizOrDie.Converters; 
+// true первый цвет в параметре
+// false второй цвет
+public class BoolToColorConverter : IValueConverter
 {
-    internal class BoolToColorConverter
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        var parts = (parameter as string)?.Split('|');
+        if (parts is not { Length: 2 }) return Colors.Transparent;
+        bool flag = value is true;
+        return Color.FromArgb(flag ? parts[0] : parts[1]);
     }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+// true 1.0 полная непрозрачность false 0.4 серый
+public class BoolToOpacityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? 1.0 : 0.35;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+// инвертирует bool
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is not true;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is not true;
 }
